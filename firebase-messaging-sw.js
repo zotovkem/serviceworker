63ccc7ce89a01732f1ddb1830b0e6593d9e923bd
Peter Gribanov <wcode404@gmail.com>
2017-03-15 17:25:08 +0300

    add full firebase config

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,13 @@
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
-firebase.initializeApp({
-    'messagingSenderId': '448358493027'
-});
+var config = {
+    apiKey: "AIzaSyDsgrxryBjQrS38vdxuDk_3RRJtuOGbU1c",
+    authDomain: "test-f24b5.firebaseapp.com",
+    databaseURL: "https://test-f24b5.firebaseio.com",
+    storageBucket: "test-f24b5.appspot.com",
+    messagingSenderId: "448358493027"
+};
+firebase.initializeApp(config);
 
 firebase.messaging();
