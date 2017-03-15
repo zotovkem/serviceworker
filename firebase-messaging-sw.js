8e6d547a2ee60e07333ca303058f28ea201aa80e
Peter Gribanov <wcode404@gmail.com>
2017-03-15 17:38:44 +0300

    revert changes

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,8 @@
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
-var config = {
-    apiKey: "AIzaSyDsgrxryBjQrS38vdxuDk_3RRJtuOGbU1c",
-    authDomain: "test-f24b5.firebaseapp.com",
-    databaseURL: "https://test-f24b5.firebaseio.com",
-    storageBucket: "test-f24b5.appspot.com",
-    messagingSenderId: "448358493027"
-};
-firebase.initializeApp(config);
+ firebase.initializeApp({
+     'messagingSenderId': '448358493027'
+ });
 
 firebase.messaging();
