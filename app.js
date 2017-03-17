40912858f54b9e1d04de0e0ec0dd7fd5b38c35b4
Peter Gribanov <wcode404@gmail.com>
2017-03-17 18:40:29 +0300

    remove seconds from notification body

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ var alert = $('#alert');
 var alert_message = $('#alert-message');
 
 var input_body = $('#body');
-var timerId = setInterval(setNotificationDemoBody, 5000);
+var timerId = setInterval(setNotificationDemoBody, 10000);
 
 function setNotificationDemoBody() {
-    if (input_body.val().search(/^It's found today at \d\d:\d\d:\d\d$/i) !== -1) {
+    if (input_body.val().search(/^It's found today at \d\d:\d\d$/i) !== -1) {
         var now = new Date();
-        input_body.val('It\'s found today at ' + now.getHours() + ':' + addZero(now.getMinutes()) + ':' + addZero(now.getSeconds()));
+        input_body.val('It\'s found today at ' + now.getHours() + ':' + addZero(now.getMinutes()));
     } else {
         clearInterval(timerId);
     }
