08f17fa6c949339a4e1f1e1aeb628f64898ebbe8
Peter Gribanov <wcode404@gmail.com>
2017-03-17 17:35:38 +0300

    try simple open window

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -8,22 +8,23 @@ firebase.initializeApp({
 firebase.messaging();
 
 self.addEventListener('notificationclick', function(event) {
-    var target = event.notification.data.click_action || '/';
+    const target = event.notification.data.click_action || '/';
     event.notification.close();
 
+    event.waitUntil(clients.openWindow(target));
     // This looks to see if the current is already open and focuses if it is
-    event.waitUntil(clients.matchAll({
-        type: 'window',
-        includeUncontrolled: true
-    }).then(function(clientList) {
-        // clientList always is empty?!
-        for (var i = 0; i < clientList.length; i++) {
-            var client = clientList[i];
-            if (client.url == target && 'focus' in client) {
-                return client.focus();
-            }
-        }
-
-        return clients.openWindow(target);
-    }));
+    // event.waitUntil(clients.matchAll({
+    //     type: 'window',
+    //     includeUncontrolled: true
+    // }).then(function(clientList) {
+    //     // clientList always is empty?!
+    //     for (var i = 0; i < clientList.length; i++) {
+    //         var client = clientList[i];
+    //         if (client.url == target && 'focus' in client) {
+    //             return client.focus();
+    //         }
+    //     }
+    //
+    //     return clients.openWindow(target);
+    // }));
 });
