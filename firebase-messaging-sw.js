8dae22bea2049d6a3b2fb41dc90cbf3e9f23c447
Peter Gribanov <wcode404@gmail.com>
2017-03-17 11:45:41 +0300

    update firebase lib

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
 
  firebase.initializeApp({
      'messagingSenderId': '448358493027'
