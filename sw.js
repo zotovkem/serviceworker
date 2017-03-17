d329c4e3fd4072455a903f177fee55c471386af9
Peter Gribanov <wcode404@gmail.com>
2017-03-17 15:11:04 +0300

    try use simple code for handle notification click

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
 self.addEventListener('notificationclick', function(event) {
     var target = event.notification.data.click_action || '/';
     event.notification.close();
+    event.waitUntil(self.clients.openWindow(target));
 
     // This looks to see if the current is already open and focuses if it is
-    event.waitUntil(clients.matchAll({
-        type: 'window'
-    }).then(function(clientList) {
-        // clientList always is empty?!
-        for (var i = 0; i < clientList.length; i++) {
-            var client = clientList[i];
-            if (client.url == target && 'focus' in client) {
-                return client.focus();
-            }
-        }
-
-        if (clients.openWindow) {
-            return clients.openWindow(target);
-        }
-    }));
+    // event.waitUntil(clients.matchAll({
+    //     type: 'window'
+    // }).then(function(clientList) {
+    //     // clientList always is empty?!
+    //     for (var i = 0; i < clientList.length; i++) {
+    //         var client = clientList[i];
+    //         if (client.url == target && 'focus' in client) {
+    //             return client.focus();
+    //         }
+    //     }
+    //
+    //     if (clients.openWindow) {
+    //         return clients.openWindow(target);
+    //     }
+    // }));
 });
