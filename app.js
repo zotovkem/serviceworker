2074f6f09d3d243cc67373d65d63e4cc17745bd0
Peter Gribanov <wcode404@gmail.com>
2018-06-09 19:20:17 +0300

    correct log notification data

--- app.js
+++ app.js
@@ -92,8 +92,8 @@ if (window.location.protocol === 'https:' &&
         info.show();
         info_message
             .text('')
-            .append('<strong>'+payload.notification.title+'</strong>')
-            .append('<em> '+payload.notification.body+'</em>')
+            .append('<strong>'+payload.data.title+'</strong>')
+            .append('<em> '+payload.data.body+'</em>')
         ;
 
         // register fake ServiceWorker for show notification on mobile devices
