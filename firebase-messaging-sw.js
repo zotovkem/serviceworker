89091f6e7269bc4dc2aada5a9964d21bf9ec85c7
zotovkem <zotov_86@mail.ru>
2019-08-26 12:13:23 +0700

    Update firebase-messaging-sw.js
    Изменил версию lib получения уведомления

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-messaging.js');
 
 firebase.initializeApp({
   messagingSenderId: '172176437227'
